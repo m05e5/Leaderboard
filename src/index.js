ab9d802b139ee98f9e17cdc8481254e59f2529fd
m05e5 <calebmoses1111@gmail.com>
2021-08-03 19:25:30 +0100

    add api calls to project

--- src/index.js
+++ src/index.js
@@ -4,20 +4,58 @@ import _ from 'lodash';
 import './style.css';
 import Scores from './scores';
 
-const updateScore = () => {
-  const myScores = new Scores();
+const request = new XMLHttpRequest();
+const requestURL = 'https://us-central1-js-capstone-backend.cloudfunctions.net/api/games/e7Fuv9HQd45w43wbbQ4v/scores';
+const refreshBtn = document.getElementById('refreshBtn');
+const formSubmit = document.getElementById('form-submit');
+
+const myScores = new Scores();
+
+const drawScore = (scoress) => {
   let i = 0;
   const scores = document.getElementById('scores');
+  const liToRemove = document.querySelectorAll('#scores li');
+  liToRemove.forEach((item) => {
+    item.remove();
+  });
   scores.classList.add('black-border');
-  myScores.scoreList.forEach((score) => {
+  scoress.scoreList.forEach((score) => {
     const li = document.createElement('li');
     if (i % 2 === 0) {
       li.className = 'dark-bg';
     }
     i += 1;
     li.classList.add('score');
-    li.innerHTML = `<p>${score.name}: ${score.score}</p>`;
+    li.innerHTML = `<p>${score.user}: ${score.score}</p>`;
     scores.appendChild(li);
   });
 };
-updateScore();
+
+formSubmit.onclick = (e) => {
+  e.preventDefault();
+  const inputName = document.getElementById('name');
+  const inputScore = document.getElementById('score');
+  const params = `user=${inputName.value}&score=${inputScore.value}`;
+  request.open('POST', requestURL, true);
+  request.setRequestHeader('Content-type', 'application/x-www-form-urlencoded');
+  request.onreadystatechange = function () {
+    if (request.readyState == 4 && request.status == 200) {
+      alert(request.responseText);
+    }
+  };
+  request.send(params);
+};
+
+myScores.getScore();
+drawScore(myScores);
+
+refreshBtn.onclick = async () => {
+  await myScores.getScore();
+  drawScore(myScores);
+};
+
+const init = async () => {
+  await myScores.getScore();
+  drawScore(myScores);
+};
+onload = init();
--- src/scores.js
+++ src/scores.js
@@ -1,22 +1,25 @@
+const request = new XMLHttpRequest();
+const requestURL = 'https://us-central1-js-capstone-backend.cloudfunctions.net/api/games/e7Fuv9HQd45w43wbbQ4v/scores';
+
 export default class Scores {
   constructor() {
-    this.scoreList = [
-      {
-        name: 'One',
-        score: 20,
-      },
-      {
-        name: 'two',
-        score: 22,
-      },
-      {
-        name: 'three',
-        score: 60,
-      },
-      {
-        name: 'four',
-        score: 35,
-      },
-    ];
+    this.scoreList = [];
+  }
+
+  async getScore() {
+    const scorePromis = new Promise((myResolve, myReject) => {
+      request.open('GET', requestURL);
+      request.onload = () => {
+        if (request.status == 200) {
+          myResolve(request.response);
+          console.log(request.response);
+        } else {
+          myResolve('Error');
+        }
+      };
+      request.send();
+    });
+    const list = JSON.parse(await scorePromis);
+    this.scoreList = list.result;
   }
 }
